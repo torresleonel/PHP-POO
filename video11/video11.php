1ca56d1cbbb376af246346843ee2c023d6464f3d
torresleonel <torresleonel18@gmail.com>
2015-10-16 04:12:04 -0430

    Modificado video11.php
    Se agrego el contenido practico segun lo indicado en el video N° 11

--- video11/video11.php
+++ video11/video11.php
@@ -14,6 +14,34 @@
 
 
 		<?php
+
+			trait Persona2{
+
+				public $nombre;
+
+				public function mostrarNombre(){
+					echo $this->nombre;
+				}
+
+				abstract function asignarNombre($nombre);
+			}
+
+			trait Trabajador{
+				protected function mensaje(){
+					echo 'Mi nombre es: ';
+				}
+			}
+
+			class Persona{
+				use Persona2, Trabajador;
+				public function asignarNombre($nombre){
+					$this->nombre = $this->mensaje().$nombre;
+				}
+			}
+
+			$persona = new Persona();
+			$persona->asignarNombre('Leo');
+			$persona->mostrarNombre();
 			
 		?>
 
