e28fe0524eb97d49d3accfa99be78f90955cf15a
torresleonel <torresleonel18@gmail.com>
2015-10-19 21:57:29 -0430

    PHP-POO.SQL con datos
    se agrega nuevo respaldo de la BD co datos incluidos

--- proyecto/BD/PHP-POO.sql
+++ proyecto/BD/PHP-POO.sql
@@ -4,37 +4,39 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 CREATE DATABASE IF NOT EXISTS `php-poo` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
 USE `php-poo`;
 
 CREATE TABLE IF NOT EXISTS `estudiante` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `edad` int(11) NOT NULL,
   `promedio` double NOT NULL,
   `imagen` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `id_seccion` int(11) NOT NULL,
-  `fecha` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `fecha` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_seccion` (`id_seccion`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+INSERT INTO `estudiante` (`id`, `nombre`, `edad`, `promedio`, `imagen`, `id_seccion`, `fecha`) VALUES
+(1, 'Leonel Torres', 26, 15, 'no tiene', 3, '2015-10-19 21:08:00'),
+(2, 'Juan Gonzales', 38, 18, 'Tampoco tiene', 1, '2015-10-19 21:09:00');
 
 CREATE TABLE IF NOT EXISTS `seccion` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(80) COLLATE utf8_spanish_ci NOT NULL
+  `nombre` varchar(80) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+INSERT INTO `seccion` (`id`, `nombre`) VALUES
+(1, 'a'),
+(2, 'b'),
+(3, 'c'),
+(4, 'd');
 
-ALTER TABLE `estudiante`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_seccion` (`id_seccion`);
-
-ALTER TABLE `seccion`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `estudiante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `estudiante`
   ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`id_seccion`) REFERENCES `seccion` (`id`) ON UPDATE CASCADE;
--- proyecto/index.php
+++ proyecto/index.php
@@ -1,9 +1,9 @@
 <?php
 
-	require_once 'Config/Autoload.php';
+	/*require_once 'Config/Autoload.php';
 	Config\Autoload::run();
 	$est = new Models\Estudiante();
-	$est->set('id', 1);
+	$est->set('id', 2);
 	$datos = $est->view();
-	print $datos->nombre;
+	print $datos->nombre;*/
 ?>
